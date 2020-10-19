48f61295fda4c3c6aa9ae22cd3d4480ff8dcf25c
Ekaterina199422 <69307368+Ekaterina199422@users.noreply.github.com>
2020-10-19 10:21:02 +0200

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,48 @@
 fun main() {
-    val typeCard = "Mastercard"
-    val transferAmount = 65_000_00
-    val previousTransaction = 20_000_00
-
-    val commission = calculationCommission(typeCard = typeCard, previousTransaction = previousTransaction, transferAmount = transferAmount)
-    println("При переводе: $transferAmount копеек с карты: $typeCard, комиссия составит: $commission копеек")
+    val cardType = "MasterCard"
+    val monthTransfer = 320_000_000L
+    val transfer = 2_000_000L
+    commissionAmount(cardType, monthTransfer, transfer)
 }
 
-fun calculationCommission(typeCard: String = "VK Pay", previousTransaction: Int = 0, transferAmount: Int): Int {
-    val prefLimit = 75_000_00
-    val overrunLimit = prefLimit - previousTransaction - transferAmount
-    val minCommissionForMastercardMaestro = 20 * 100
-    val commissionForMastercardMaestro = 0.006
-    val minCommissionForVisaMir = 35 * 100
-    val commissionForVisaMir = 0.0075
-    val totalCommissionForVisaMir = transferAmount * commissionForVisaMir
-
-    return when(typeCard) {
-        "Mastercard", "Maestro" -> if (overrunLimit < 0) (kotlin.math.abs(overrunLimit) * commissionForMastercardMaestro + minCommissionForMastercardMaestro).toInt() else 0
-        "Visa", "Мир" ->  if (totalCommissionForVisaMir > minCommissionForVisaMir) totalCommissionForVisaMir.toInt() else minCommissionForVisaMir
+private fun commissionAmount(
+        cardType: String = "VK Pay",
+        monthTransfer: Long = 0L,
+        transfer: Long
+): Long {
+    return when (cardType) {
+        "MasterCard", "Maestro" -> {
+            calForMasterMaestroCard(monthTransfer, transfer)
+        }
+        "Visa", "Мир" -> {
+           calForVisaMirCard(transfer)
+        }
+        "VK Pay" -> {
+            println("Комиссия не взимается")
+            0
+        }
         else -> 0
+    }
+}
+
+private fun calForVisaMirCard(transfer: Long): Long {
+    val commission = transfer * 0.0075
+    return if (commission > 3500) {
+        println("Комиссия: $commission коп")
+        commission.toLong()
+    } else {
+        println("Комиссия: 3500 коп")
+        3500
+    }
+}
+
+private fun calForMasterMaestroCard(monthTransfer: Long, transfer: Long): Long {
+    return if (monthTransfer + transfer <= 7_500_000) {
+        println("Комиссия не взимается")
+        0
+    } else {
+        val commission = transfer * 0.006 + 2000
+        println("Комиссия: $commission коп")
+        commission.toLong()
     }
 }
